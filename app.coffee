overleaf/clsi

commit 17c51c2ba0bd2556e8a9d4f3eaf11e75ff9b5c1f

added debugging and new moving commands

app.coffee
@@ -144,6 +144,10 @@ app.get "/smoke_test_force", (req, res)->
 	smokeTest.run(require.resolve(__dirname + "/test/smoke/js/SmokeTests.js"))(req, res)
 
 
+#TODO delete this
+app.get "/settings", (req, res)->
+	res.json(Settings)
+
 profiler = require "v8-profiler"
 app.get "/profile", (req, res) ->
 	time = parseInt(req.query.time || "1000")

entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 echo "Changing permissions of /var/run/docker.sock for sibling containers"
-cp /var/clsi/bin/synctex /var/clsi/bin/synctex-mount/synctex
 
 chown root:docker /var/run/docker.sock
-chown app:app /app/compiles
+
+mkdir -p /app/cache
+chown -R app:app /app/cache
+
+mkdir -p /app/compiles
+chown -R app:app /app/compiles
 
 ./bin/install_texlive_gce.sh
 exec runuser -u app "$@"

install_deps.sh
@@ -4,8 +4,5 @@ apt-get install poppler-utils vim ghostscript --yes
 npm rebuild
 usermod -aG docker app
 
-mkdir -p /app/cache
-chown -R app:app /app/cache
+cp /app/bin/synctex /app/bin/synctex-mount/synctex
 
-mkdir -p /app/compiles
-chown -R app:app /app/compiles

commit 2531b386e557f4eea17a634656ff192d1032c341

[app] drop Settings leak from the API
introduced in 17c51c2ba0bd2556e8a9d4f3eaf11e75ff9b5c1f

app.coffee
@@ -143,11 +143,6 @@ app.get "/health_check", (req, res)->
 app.get "/smoke_test_force", (req, res)->
 	smokeTest.run(require.resolve(__dirname + "/test/smoke/js/SmokeTests.js"))(req, res)
 
-
-#TODO delete this
-app.get "/settings", (req, res)->
-	res.json(Settings)
-
 profiler = require "v8-profiler"
 app.get "/profile", (req, res) ->
 	time = parseInt(req.query.time || "1000")