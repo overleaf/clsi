d70a665fc35e67a6ee4f10bd3e26c2de06198eb4
Jakob Ackermann <jakob.ackermann@overleaf.com>
2020-12-09 09:32:56 +0000

    [misc] WIP: pdf caching: improve browser caching, emit caching headers

--- app/js/ContentController.js
+++ app/js/ContentController.js
@@ -3,6 +3,9 @@ const send = require('send')
 const Settings = require('settings-sharelatex')
 const OutputCacheManager = require('./OutputCacheManager')
 
+const ONE_DAY_S = 24 * 60 * 60
+const ONE_DAY_MS = ONE_DAY_S * 1000
+
 function getPdfRange(req, res, next) {
   const { project_id: projectId, user_id: userId, hash } = req.params
   const compileDir = Path.join(
@@ -16,6 +19,8 @@ function getPdfRange(req, res, next) {
     }
     const path = Path.join(contentDir, hash)
     console.log({ path })
+    res.setHeader('cache-control', `public, max-age=${ONE_DAY_S}`)
+    res.setHeader('expires', new Date(Date.now() + ONE_DAY_MS).toUTCString())
     send(req, path).pipe(res)
   })
 }
