a9c81629daf136795cb65f154884505a1f550f77
Alf Eaton <alf.eaton@overleaf.com>
2021-02-18 10:19:27 +0000

    Try groupmod before groupadd for dockeronhost

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 docker --version >&2
 
-# add the node user to the docker group on the host
+# set the dockeronhost group id in the container to the group id from the host
 DOCKER_GROUP=$(stat -c '%g' /var/run/docker.sock)
-groupadd --non-unique --gid ${DOCKER_GROUP} dockeronhost
-usermod -aG dockeronhost node
+# try to modify an existing group id
+groupmod --non-unique --gid ${DOCKER_GROUP} dockeronhost
+# if the group doesn't exist yet, create it
+if [ $? -eq 4 ]; then groupadd --non-unique --gid ${DOCKER_GROUP} dockeronhost; fi
 
 # compatibility: initial volume setup
 chown node:node /app/cache
