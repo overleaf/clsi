c044ca1b39170651337983d9524e244578ac59eb
Tian Yunhao <t123yh@outlook.com>
2021-05-20 10:13:04 +0800

    LocalCommandRunner: fix unstoppable compilation
    The options for `spawn` operation should contain a `detached` item to make the subprocess really detached, otherwise compilation will not be stoppable.

--- app/js/LocalCommandRunner.js
+++ app/js/LocalCommandRunner.js
@@ -55,7 +55,7 @@ module.exports = CommandRunner = {
     }
 
     // run command as detached process so it has its own process group (which can be killed if needed)
-    const proc = spawn(command[0], command.slice(1), { cwd: directory, env })
+    const proc = spawn(command[0], command.slice(1), { cwd: directory, env, detached: true })
 
     let stdout = ''
     proc.stdout.setEncoding('utf8').on('data', (data) => (stdout += data))
