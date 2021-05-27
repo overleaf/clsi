63150ad11c13d1962ddd89914cd66a1c9edfe916
Jakob Ackermann <jakob.ackermann@overleaf.com>
2021-05-27 17:19:07 +0100

    [misc] abstract the file loading and parsing of xRef tables into helper

--- scripts/demo-pdfjs-Xref.js
+++ scripts/demo-pdfjs-Xref.js
@@ -1,20 +1,12 @@
-const { FSPdfManager } = require('../app/lib/pdfjs/FSPdfManager')
 const fs = require('fs')
+const { parseXrefTable } = require('../app/lib/pdfjs/parseXrefTable')
 
 const pdfPath = process.argv[2]
 
-const fh = fs.promises.open(pdfPath)
-fh.then(handle => {
-  handle.stat().then(stats => {
-    const pdfManager = new FSPdfManager(1, { fh: handle, size: stats.size }, '', {}, '')
-    pdfManager.ensureDoc('checkHeader', []).then(() => {
-      return pdfManager.ensureDoc('parseStartXRef', [])
-    }).then(() => {
-      return pdfManager.ensureDoc('parse')
-    }).then(() => {
-      console.log("Xref entries", pdfManager.pdfDocument.catalog.xref.entries)
-    })
-  })
-})
+async function main() {
+  const size = (await fs.promises.stat(pdfPath)).size
+  const xRefEntries = await parseXrefTable(pdfPath, size)
+  console.log('Xref entries', xRefEntries)
+}
 
-
+main().catch(console.error)
